768c78e302e43d0dfd68e34f42016d55f7807187
James Christensen <bornonthefirstofmay@gmail.com>
2020-05-05 14:11:29 -0400

    endpoint set

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 function rootHandler(req, res) {
-  console.log(req);
   res.render('pages/index');
+}
+function gamesHandler(req, res) {
+  res.send({});
 }
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', rootHandler)
+  .get('/games', gamesHandler)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
