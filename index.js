bbc7d7b7b21b0ba3128e3057bfceac4446f89fd8
James Christensen <bornonthefirstofmay@gmail.com>
2020-05-05 13:54:03 -0400

    added axios dependency

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+function rootHandler(req, res) {
+  console.log(req);
+  res.render('pages/index');
+}
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', rootHandler)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
