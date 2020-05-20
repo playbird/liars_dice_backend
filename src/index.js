605908295bd3772105694c07459f79d866cb1ca8
James Christensen <bornonthefirstofmay@gmail.com>
2020-05-20 10:44:07 -0400

    linked to css, added some styles

--- src/index.js
+++ src/index.js
@@ -10,13 +10,22 @@ function getGame(userID) {
 }
 
 function drawGame(document, gameState, myID) {
+  // step 1: find the element with the ID (div)
+  // step 2: gind its parent and store it
+  // step 3: remove the tagged element
+  // step 4: make a new one
+  // step 5: add the new one to the old one's parent
+  
   while (document.body.firstChild) {
     document.body.removeChild(document.body.firstChild);
   }
-  let playerRow = document.createElement('P');
+  title = document.createElement('h1');
+  title.textContent = "Liar's Dice";
+  document.body.appendChild(title);
+  playerRow = document.createElement('h2');
   playerRow.textContent = JSON.stringify(gameState);
-  document.body.appendChild(playerRow);
-}
+  document.body.appendChild(playerRow); 
+  }
 
 module.exports = {
   getGame: getGame,
