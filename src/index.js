74fead99487232248cfca16dfea5029b26effcbe
James Christensen <bornonthefirstofmay@gmail.com>
2020-05-20 11:19:27 -0400

    button, button, we added a button

--- src/index.js
+++ src/index.js
@@ -11,21 +11,23 @@ function getGame(userID) {
 
 function drawGame(document, gameState, myID) {
   // step 1: find the element with the ID (div)
-  // step 2: gind its parent and store it
+  // step 2: find its parent and store it
   // step 3: remove the tagged element
   // step 4: make a new one
   // step 5: add the new one to the old one's parent
-  
   while (document.body.firstChild) {
     document.body.removeChild(document.body.firstChild);
   }
-  title = document.createElement('h1');
-  title.textContent = "Liar's Dice";
-  document.body.appendChild(title);
   playerRow = document.createElement('h2');
   playerRow.textContent = JSON.stringify(gameState);
   document.body.appendChild(playerRow); 
-  }
+  
+  let rerollButton = window.document.createElement('a');
+  document.body.appendChild(rerollButton);
+  rerollButton.textContent = "Re-roll";
+  rerollButton.href = "#";
+  rerollButton.onclick = reRoll;
+}
 
 module.exports = {
   getGame: getGame,
